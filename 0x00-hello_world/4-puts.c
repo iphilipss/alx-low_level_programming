f4680a9e8294a2b6970cf48e2be0056121a4684b
iphilipss <abasifamke@gmail.com>
2022-09-12 12:44:08 -0700

    ohhhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point  
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"programming is like buiding a multilingual puzzele");
+	return (0);
 }
